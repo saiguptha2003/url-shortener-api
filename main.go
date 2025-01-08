098832e74424d975c0432cb4143acb9931b9ce43
pandu sai <saiguptha_v@srmap.edu.in>
2025-01-08 15:30:31 +0530

    modified the yaml file and the docker for image verification

--- main.go
+++ main.go
@@ -11,13 +11,24 @@ import (
 func SetupRouter() *gin.Engine {
 	urlShortener := models.NewURLShortener()
 	r := gin.Default()
-
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the URL Shortener API",
+			"endpoints": []string{
+				"/shorten - POST request to shorten a URL",
+				"/:shortURL - GET request to redirect to the original URL",
+				"/top-domains - GET request to fetch top domains",
+			},
+			"note": "Use /shorten to shorten URLs. For more details, check the other endpoints.",
+		})
+	})
 	r.POST("/shorten", func(c *gin.Context) {
 		handlers.ShortenURL(c, urlShortener)
 	})
 	r.GET("/:shortURL", func(c *gin.Context) {
 		handlers.RedirectToOriginalURL(c, urlShortener)
 	})
+
 	r.GET("/top-domains", func(c *gin.Context) {
 		handlers.GetTopDomains(c, urlShortener)
 	})
@@ -28,5 +39,5 @@ func SetupRouter() *gin.Engine {
 
 func main() {
 	r := SetupRouter()
-	r.Run(":8080")
+	r.Run(":5000")
 }
