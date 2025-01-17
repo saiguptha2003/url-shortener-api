52f1ec12406a65318b755d630d32fb46f6d0d5e0
pandu sai <saiguptha_v@srmap.edu.in>
2025-01-17 16:24:23 +0530

    updated with logtime modification

--- handlers/redirectUrl.go
+++ handlers/redirectUrl.go
@@ -2,19 +2,27 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 	"url-shortener-api/models"
-
 	"github.com/gin-gonic/gin"
 )
 
 func RedirectToOriginalURL(c *gin.Context, urlShortener *models.URLShortener) {
 	shortURL := c.Param("shortURL")
 
-	urlShortener.Mutex.RLock()
-	defer urlShortener.Mutex.RUnlock()
+	urlShortener.Mutex.Lock()
+	defer urlShortener.Mutex.Unlock()
 
 	// Redirect if the short URL exists
 	if originalURL, exists := urlShortener.Store[shortURL]; exists {
+		// Update HitOrNot in the log entry
+		for i := range urlShortener.LogData {
+			if urlShortener.LogData[i].ShortenUrl == shortURL {
+				urlShortener.LogData[i].CreatedAt = time.Now().Format(time.RFC3339) // Reset timestamp
+				break
+			}
+		}
+
 		c.Redirect(http.StatusMovedPermanently, originalURL)
 		return
 	}
