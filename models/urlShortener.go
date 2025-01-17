3a3a6665c5bfdc0fba1c37d440596c62f62d2e3c
pandu sai <saiguptha_v@srmap.edu.in>
2025-01-17 16:25:39 +0530

    updated with Log structure in instance function

--- models/urlShortener.go
+++ models/urlShortener.go
@@ -2,17 +2,27 @@ package models
 
 import (
 	"sync"
+
 )
 
 type URLShortener struct {
 	Store      map[string]string
 	ClickStore map[string]int
 	Mutex      sync.RWMutex
+	LogData    []Log   
+
+}
+type Log struct {
+	CreatedAt string
+	ShortenUrl string
+
 }
 
 func NewURLShortener() *URLShortener {
 	return &URLShortener{
 		Store:      make(map[string]string),
 		ClickStore: make(map[string]int),
+		LogData:    []Log{},
 	}
 }
+
